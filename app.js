5d1a09f6806eead44f4969be5f662fcf2ed074ca
mladendjokovic <mldndjkvc@gmail.com>
2020-04-27 05:01:19 +0200

    added close icon to navbar

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const navToggle = document.querySelector('.toggle-icon')
 const hamburger = document.querySelector('.hamburger-icon');
 const close = document.querySelector('.close-icon');
 const nav = document.querySelector('ul');
-hamburger.addEventListener('click', function() {
+navToggle.addEventListener('click', function() {
     nav.classList.toggle('show-nav');
+    hamburger.classList.toggle('hide');
+    close.classList.toggle('show');
 });
